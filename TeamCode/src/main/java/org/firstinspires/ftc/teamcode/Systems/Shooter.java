d2d67eca8610b5a8d349652e5eee69a07d41de54
Robopapiu <bogdancezarrares@gmail.com>
2025-11-04 21:39:58 +0200

    Org

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Systems;
 
 import android.service.controls.Control;
 
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BangBang;
+import com.ThermalEquilibrium.homeostasis.Filters.FilterAlgorithms.KalmanFilter;
+import com.ThermalEquilibrium.homeostasis.Parameters.BangBangParameters;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -20,10 +23,18 @@ public class Shooter {
     public DcMotorEx SS,SD;
     public Servo SVS,SVD;
     public Telemetry telemetry;
+    public double target;
     public boolean pornit=false,yea=false;
     public double p,i,d;
-    public double output,target;
-    public ControlSystem pid;
+    double maxOutput = 1;
+    double tolerance = 3;
+    double command,estimate;
+    double Q = 0.3; // High values put more emphasis on the sensor.
+    double R = 3; // High Values put more emphasis on regression.
+    int N = 3; // The number of estimates in the past we perform regression on.
+    BangBangParameters parameters = new BangBangParameters(maxOutput,tolerance);
+    KalmanFilter filter = new KalmanFilter(Q,R,N);
+    BangBang controller = new BangBang(parameters);
     public Shooter(HardwareMap hardwareMap, Telemetry telemetry){
 
         SS=hardwareMap.get(DcMotorEx.class, "SS");
@@ -35,19 +46,15 @@ public class Shooter {
         SVD.setPosition(0.5);
         SD.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        pid = ControlSystem.builder()
-                .velPid(p,i,d)
-                .build();
     }
     public void periodic(){
         run();
     }
     public void run(){
-        output = pid.calculate(
-               new KineticState(SS.getCurrentPosition(), target,20)
-        );
-        SS.setPower(output);
-        SD.setPower(output);
+        command = controller.calculate(target, SD.getVelocity());
+        estimate = filter.estimate(command);
+        SD.setPower(command);
+        SS.setPower(command);
     }
     public void hoodfar(){
         SVD.setPosition(0.75);
