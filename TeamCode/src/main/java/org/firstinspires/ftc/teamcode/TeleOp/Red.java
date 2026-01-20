90170c329e79c5181b68812c8e4901764483744e
Robopapiu <bogdancezarrares@gmail.com>
2026-01-20 21:41:13 +0200

    Org

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Red.java
@@ -66,11 +66,12 @@ public class Red extends OpMode {
         r.tPeriodic();
         t.addData("Velocity: ", r.s.getVelocity());
         t.addData("Dist: ", r.dist);
-        t.addData("Turret Ticks", r.tu.getTurret());
-        t.addData("Follower Pose", r.f.getPose().toString());
-        t.addData("Loop time", r.getLoopTimeMs());
-        t.addData("Loop time hz", r.getLoopTimeHz());
-        t.addData("Intake Velocity", r.i.getVelocity());
+        t.addData("Shooter offset: ", r.s.offset);
+        t.addData("Turret Ticks: ", r.tu.getTurret());
+        t.addData("Follower Pose: ", r.f.getPose().toString());
+        t.addData("Loop time: ", r.getLoopTimeMs());
+        t.addData("Loop time hz: ", r.getLoopTimeHz());
+        t.addData("Intake Velocity: ", r.i.getVelocity());
         t.update(telemetry);
     }
 }
